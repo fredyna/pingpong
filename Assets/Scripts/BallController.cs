1d319d0acccfcba9209967674f2ee369ab1c7244
Fredy Nur Apriyanto <mail.fredyna@gmail.com>
2019-06-30 17:12:33 +0700

    first upload

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    public int scoreP1;
+    public int scoreP2;
+    public int force;
+    Rigidbody2D rigid;
+    Text scoreUIP1;
+    Text scoreUIP2;
+    GameObject panelSelesai;
+    Text txtpemenang;
+    AudioSource audio;
+    public AudioClip hitSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        Vector2 arah = new Vector2(2, 0).normalized;
+        rigid.AddForce(arah * force);
+        scoreP1 = 0;
+        scoreP2 = 0;
+        scoreUIP1 = GameObject.Find("Score1").GetComponent<Text>();
+        scoreUIP2 = GameObject.Find("Score2").GetComponent<Text>();
+        panelSelesai = GameObject.Find("PanelSelesai");
+        panelSelesai.SetActive(false);
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ResetBall()
+    {
+        transform.localPosition = new Vector2(0, 0);
+        rigid.velocity = new Vector2(0, 0);
+    }
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.name == "TepiKanan")
+        {
+            ResetBall();
+            Vector2 arah = new Vector2(2, 0).normalized;
+            rigid.AddForce(arah * force);
+        }
+        if (coll.gameObject.name == "TepiKiri")
+        {
+            ResetBall();
+            Vector2 arah = new Vector2(-2, 0).normalized;
+            rigid.AddForce(arah * force);
+        }
+        if (coll.gameObject.name == "Pemukul1" || coll.gameObject.name == "Pemukul2")
+        {
+            float sudut = (transform.position.y - coll.transform.position.y) * 5f;
+            Vector2 arah = new Vector2(rigid.velocity.x, sudut).normalized;
+            rigid.velocity = new Vector2(0, 0);
+            rigid.AddForce(arah * force * 2);
+        }
+        if (coll.gameObject.name == "TepiKanan")
+        {
+            scoreP1 += 1;
+            TampilkanScore();
+            if (scoreP1 == 5)
+            {
+                panelSelesai.SetActive(true);
+                txtpemenang = GameObject.Find("Pemenang").GetComponent<Text>();
+                txtpemenang.text = "Player Biru Pemenang!";
+                Destroy(gameObject);
+                return;
+            }
+            ResetBall();
+            Vector2 arah = new Vector2(2, 0).normalized;
+            rigid.AddForce(arah * force);
+        }
+        if (coll.gameObject.name == "TepiKiri")
+        {
+            scoreP2 += 1;
+            TampilkanScore();
+            if (scoreP2 == 5)
+            {
+                panelSelesai.SetActive(true);
+                txtpemenang = GameObject.Find("Pemenang").GetComponent<Text>();
+                txtpemenang.text = "Player Merah Pemenang!";
+                Destroy(gameObject);
+                return;
+            }
+            ResetBall();
+            Vector2 arah = new Vector2(-2, 0).normalized;
+            rigid.AddForce(arah * force);
+        }
+        audio.PlayOneShot(hitSound);
+    }
+
+    void TampilkanScore()
+    {
+        Debug.Log("Score P1 : " + scoreP1 + " Score P2 : " + scoreP2);
+        scoreUIP1.text = scoreP1 + "";
+        scoreUIP2.text = scoreP2 + "";
+    }
+}
---
+++ Assets/Scripts/HalamanManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HalamanManager : MonoBehaviour
+{
+    public bool isEscapeToExit;
+    GameObject panelCredit;
+
+    // Use this for initialization
+    void Start()
+    {
+        panelCredit = GameObject.Find("PanelCredit");
+        panelCredit.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (isEscapeToExit)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                KembaliKeMenu();
+            }
+        }
+    }
+
+    public void MulaiPermainan()
+    {
+        SceneManager.LoadScene("Main");
+    }
+    public void KembaliKeMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void ShowCredit()
+    {
+        panelCredit.SetActive(true);
+    }
+
+    public void HideCredit()
+    {
+        panelCredit.SetActive(false);
+    }
+}
